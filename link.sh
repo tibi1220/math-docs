f9c7ecafe3b2fd4835da66060b653ebf83b0f7f6
Tibor Sándor <tiborsandor2001@gmail.com>
2023-07-18 01:33:09 +0200

    Fix path

--- link.sh
+++ link.sh
@@ -2,16 +2,16 @@
 
 # if macos
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  tex=~/Library/texmf/tex/latex/local/
+  tex=~/Library/texmf/tex/latex/local
 else
-  tex=~/texmf/tex/latex/local/
+  tex=~/texmf/tex/latex/local
 fi
 
 mkdir -p $tex
 
-for file in config/*; do
-  f=$(basename $file)
-  b=${f%%.*}
+for file in config/*; do # config/file.cls
+  f=$(basename $file)    # file.cls§
+  b=${f%%.*}             # file
 
   rm -rf $tex/$b
   mkdir -p $tex/$b
